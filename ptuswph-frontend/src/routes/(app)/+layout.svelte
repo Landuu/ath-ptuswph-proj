4ce3bf39e17a1ccbec02bca7f6d3c4ddd35d53b6
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2023-01-15 12:57:01 +0100

    load function test

---
+++ ptuswph-frontend/src/routes/(app)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../../app.css";
+</script>
+
+<header>
+    <div class="w-full bg-gray-100">
+        Test
+    </div>
+</header>
+
+<slot />
--- ptuswph-frontend/src/routes/+layout.svelte
+++ ptuswph-frontend/src/routes/+layout.svelte
@@ -1,11 +1,1 @@
-<script>
-    import "../app.css";
-</script>
-
-<header>
-    <div class="w-full bg-gray-100">
-        Test
-    </div>
-</header>
-
 <slot />
